d1a0f30b7b369f935d601a4659d18eff5a311336
Eric <ericlg.link@gmail.com>
2022-11-08 19:53:22 -0800

    Create event models, I'm sure I'll have to come back to this

---
+++ models/event.js
@@ -0,0 +1,35 @@
+const { Sequelize, DataTypes, Model} = require ('sequelize')
+const sequelize = new Sequelize(process.env.PG_URI)
+
+class Event extends Model{}
+
+Event.init({
+    event_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+}, {
+    sequelize,
+    modelName: 'Event',
+    tableName: 'events',
+    timestamps: false
+})
+
+module.exports = Event
