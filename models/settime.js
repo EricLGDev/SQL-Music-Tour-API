bb3b85f445424a8c4d346ba1a4052940c6e24749
Eric <ericlg.link@gmail.com>
2022-11-13 10:46:23 -0800

    More associations

--- models/settime.js
+++ models/settime.js
@@ -9,8 +9,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Band, Event, Stage }) {
+      SetTime.belongsTo(Band, {
+        foreignKey: "band_id",
+        as: "band"
+      })
     }
   }
   SetTime.init({
