1d4a76e1b6c64edbe19e1584cb534e410aa32c6a
Eric <ericlg.link@gmail.com>
2022-11-13 03:24:23 -0800

    Find all events route built, postman tested

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -5,3 +5,18 @@ const { Event, MeetGreet, SetTime, Stage, Band } = db
 const { Op } = require('sequelize')
 
 // FIND ALL EVENTS
+events.get('/', async (req, res) => {
+    try{
+        const foundEvents = await Event.findAll({
+            order: [ [ 'date', 'ASC' ] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
+        })
+        res.status(200).json(foundEvents)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = events
