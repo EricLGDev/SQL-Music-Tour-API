17482b67094b8b7e5f8d7b8fadc060a355998deb
Eric <ericlg.link@gmail.com>
2022-11-13 10:14:32 -0800

    Still building the event controller, starting associations

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -19,4 +19,57 @@ events.get('/', async (req, res) => {
     }
 })
 
+// FIND A SPECIFIC EVENT
+events.get('/:name', async (req, res) => {
+    try {
+        const foundEvent = await Event.findOne({
+            where: { name: req.params.name },
+            include: [
+                {
+                    model: MeetGreet,
+                    as: "meet_greets",
+                    attributes: { exclude: [ "event_id", "band_id" ] },
+                    include: {
+                        model: Band,
+                        as: "band"
+                    }
+                },
+                {
+                    model: SetTime,
+                    as: "set_times",
+                    attributes: { exclude: [ "event_id", "stage_id", "band_id" ] },
+                    include: [
+                        { model: Band, as: "band" },
+                        { model: Stage, as: "stage" }
+                    ]
+                },
+                {
+                    model: Stage,
+                    as: "stages",
+                    through: { attributes: [] }
+                }
+            ]
+        })
+        res.status(200).json(foundEvent)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+// CREATE AN EVENT
+events.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body)
+        res.status(200).json({
+            message: 'Successfully created a new event',
+            data: newEvent
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE EVENT
+
+// DELETE EVENT
+
 module.exports = events
