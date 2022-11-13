c5a9b26731be98ef2021cdce138e745da754e08e
Eric <ericlg.link@gmail.com>
2022-11-13 11:11:49 -0800

    Finishing the stage controller

--- controllers/stage_controller.js
+++ controllers/stage_controller.js
@@ -2,3 +2,30 @@ const stages = require('express').Router()
 const db = require('../models')
 const { Stage, Event } = db 
 const { Op } = require('sequelize')
+
+// FIND ALL STAGES
+stages.get('/', async (req, res) => {
+    try {
+        const foundStages = await Stage.findAll({
+            where: {
+                stage_name: { [Op.like]: `%${req.query.stage_name ? req.query.stage_name : ''}%` }
+            }
+        })
+        res.status(200).json(foundStages)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE A STAGE
+stages.post('/', async (req, res) => {
+    try {
+        const newStage = await Stage.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new stage',
+            data: newStage
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
